bd3fa57ab3c869dc695053b5edd2b54fed0fc74d
Daniel Barbieri <dibarbieri21@gmail.com>
2026-01-12 20:55:13 -0300

    fix: melhorar mock do Toast - usar React.createElement para compatibilidade
    - Mock agora usa React.createElement('View') ao invés de null
    - Adicionado module.exports.default para compatibilidade com imports
    - Métodos estáticos show/hide/error/success/info funcionais
    - Testes continuam 18/18 passando localmente
    - Deve resolver erro de build no GitHub Actions

--- __mocks__/react-native-toast-message.js
+++ __mocks__/react-native-toast-message.js
@@ -1,13 +1,25 @@
 /**
  * Mock do react-native-toast-message para testes
  */
+const React = require('react');
 
-const ToastComponent = () => null;
+const Toast = {
+  show: jest.fn(),
+  hide: jest.fn(),
+  success: jest.fn(),
+  error: jest.fn(),
+  info: jest.fn(),
+};
 
-ToastComponent.show = jest.fn();
-ToastComponent.hide = jest.fn();
-ToastComponent.success = jest.fn();
-ToastComponent.error = jest.fn();
-ToastComponent.info = jest.fn();
+// Componente React válido que não renderiza nada
+const ToastComponent = () => React.createElement('View', null);
+
+// Adicionar métodos estáticos ao componente
+ToastComponent.show = Toast.show;
+ToastComponent.hide = Toast.hide;
+ToastComponent.success = Toast.success;
+ToastComponent.error = Toast.error;
+ToastComponent.info = Toast.info;
 
 module.exports = ToastComponent;
+module.exports.default = ToastComponent;
